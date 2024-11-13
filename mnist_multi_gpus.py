fafc1b046ab5dcf394796ee850574038c6885884
Jing Gong <ausaaronyl@gmail.com>
2024-11-13 18:07:10 +1100

    add mnist_multi_gpus

---
+++ mnist_multi_gpus.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from python.keras.layers.am_convolutional import AMConv2D
+from python.keras.layers.amdenselayer import denseam
+
+tf.random.set_seed(0)
+
+# Set up data loading
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+# Configure dataset for performance
+lut_file = "lut/MIT_7.bin"
+ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+# Set up multi-GPU training
+strategy = tf.distribute.MirroredStrategy()
+
+with strategy.scope():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=(28, 28, 1)),
+        AMConv2D(filters=32, kernel_size=5, padding='same', activation='relu', mant_mul_lut=lut_file),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        AMConv2D(filters=32, kernel_size=5, padding='same', activation='relu', mant_mul_lut=lut_file),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        tf.keras.layers.Flatten(),
+        denseam(1024, activation='relu', mant_mul_lut=lut_file),
+        tf.keras.layers.Dropout(0.4),
+        denseam(10, activation='softmax', mant_mul_lut=lut_file)
+    ])
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+# Train the model with distributed strategy
+model.fit(
+    ds_train,
+    epochs=2,
+    validation_data=ds_test,
+)
